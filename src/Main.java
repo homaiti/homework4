d270832d84a0862f7957b55fbf25af2fc960ecd9
Mikhail Yakovlev <homaiti@gmail.com>
2023-01-30 19:05:15 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("");
+        System.out.println("Задача 1");
+
+        int age = 17;
+        if(age >= 18)
+        {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        }
+        else
+        {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, " +
+                    "нужно немного подождать");
+        }
+        System.out.println();
+
+
+
     }
 }
