feb620a1a662a92e8b16fd40ab74f18c20eb9d01
Mikhail Yakovlev <homaiti@gmail.com>
2023-01-31 23:40:44 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
         }
-        else
+        if (age < 18)
         {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, " +
                     "нужно немного подождать");
@@ -16,7 +16,7 @@ public class Main {
         System.out.println();
 
         System.out.println("Задача 2");
-        double temp = 4;
+        double temp = 6;
 
         if(temp < 5)
         {
@@ -30,7 +30,7 @@ public class Main {
 
         System.out.println("Задача 3");
 
-        double speed = 61;
+        int speed = 61;
 
         if(speed > 60)
         {
@@ -48,7 +48,6 @@ public class Main {
         boolean ageTwoSix = ageMan >= 2 && ageMan <= 6;
         boolean ageSevenEighteen = ageMan >= 7 && ageMan < 18;
         boolean ageEighteenTwentyFour = ageMan >= 18 && ageMan < 24;
-        boolean ageMoreTwentyFour = ageMan >= 24;
         String error = "";
 
         if(ageTwoSix)
@@ -56,26 +55,50 @@ public class Main {
             System.out.println("Если возраст человека равен " + ageMan + ", то ему нужно ходить в детский сад");
         }
 
-        if(ageSevenEighteen)
+        else if(ageSevenEighteen)
         {
             System.out.println("Если возраст человека равен " + ageMan + ", то ему нужно ходить в школу");
         }
 
-        if(ageEighteenTwentyFour)
+        else if(ageEighteenTwentyFour)
         {
             System.out.println("Если возраст человека равен " + ageMan + ", то его место в университете");
         }
 
-        if(ageMoreTwentyFour)
+        else
         {
             System.out.println("Если возраст человека равен " + ageMan + ", то ему пора ходить на работу");
         }
+
+        System.out.println();
+
+        System.out.println("Задача 5");
+
+        int yourAge = 4;
+        boolean lessFiveYears = yourAge < 5;
+        boolean moreFiveYearsLessFourteen = yourAge >= 5 && yourAge < 14;
+        boolean moreFourteen = yourAge >= 14;
+        boolean withAdult = true;
+
+        if(lessFiveYears)
+        {
+            System.out.println("Если возраст ребенка равен " + yourAge +
+                    ", то ему нельзя кататься на аттракционе");
+        }
+        else if (moreFiveYearsLessFourteen && withAdult)
+        {
+            System.out.println("Если возраст ребенка равен " + yourAge +
+                    ", то ему можно кататься на аттракционе в сопровождении взрослого");
+        }
         else
         {
-            error = "Значение не корректно";
+            System.out.println("Если возраст ребенка равен " + yourAge +
+                    ", то ему можно кататься на аттракционе без сопровождения взрослого");
         }
 
-        System.out.println(error);
+        System.out.println();
+
+
 
     }
 }
